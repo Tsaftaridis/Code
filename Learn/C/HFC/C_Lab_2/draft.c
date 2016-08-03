a7d628c3bf7245e35d6e3d519e91ff7355deef9d
Tsaftaridis <tsaftaridis1@gmail.com>
2016-08-03 10:22:49 +0300

    C_Lab_2
    C_Lab_2 continued

--- Learn/C/HFC/C_Lab_2/draft.c
+++ Learn/C/HFC/C_Lab_2/draft.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <opencv/cv.h>
+#include <unistd.h>
 #include <opencv/cxcore.h>
 #include <opencv/highgui.h>
 #include "error_handling.h"
@@ -8,38 +9,53 @@ int main()
 {
 	// create CvCapture struct as a handle to the webcam - with error checking
 	CvCapture* webcam = cvCreateCameraCapture(0);
-	IplImage* image;
+	IplImage* prev, *next;
 	
 	if(webcam == NULL)
 		error("Can't connect to webcam");
 	
 	while(1)
 	{
-		image = cvQueryFrame(webcam);
-
+		prev = cvQueryFrame(webcam);
+		sleep(1);
+		next = cvQueryFrame(webcam);
 		
-		if(image)
+		if(prev && next)
 		{
-			printf("Done!");
-			IplImage *im_gray = cvCreateImage(cvGetSize(image),IPL_DEPTH_8U,1);
-			cvCvtColor(image, im_gray, CV_RGB2GRAY);
+			IplImage *im_gray_prev = cvCreateImage(cvGetSize(prev),IPL_DEPTH_8U,1);
+			IplImage *im_gray_next = cvCreateImage(cvGetSize(next), IPL_DEPTH_8U,1);
+			cvCvtColor(prev, im_gray_prev, CV_RGB2GRAY);
+			cvCvtColor(next, im_gray_next, CV_RGB2GRAY);
+			cvNamedWindow("Grayscale-prev", CV_WINDOW_AUTOSIZE);
+			cvNamedWindow("Grayscale-next", CV_WINDOW_AUTOSIZE);
+			cvShowImage("Grayscale-prev", im_gray_prev);
+			cvWaitKey(0);
+			cvShowImage("Grayscale-next", im_gray_next);
+			cvWaitKey(0);
+			cvReleaseImage(&im_gray_prev);
+			cvReleaseImage(&im_gray_prev);
+			cvDestroyWindow("Grayscale-prev");			
+			cvDestroyWindow("Grayscale-next");
 			
-			printf("too!");
-			/*/process image
-			printf("Got it!\n");
-			cvCvtColor(image, previous, CV_RGB2GRAY);
-			cvCvtColor(cvQueryFrame(webcam), image, CV_RGB2GRAY);
-			cvCalcOpticalFlowFarneback(previous, image, flow, 0.5, 1, 5, 5, 7, 1.5,  0);*/
-			return 0;
+			// Buggy code starts here:
+			IplImage *flow = cvCreateImage(cvGetSize(prev), IPL_DEPTH_8U, 3);
+			IplImage *flowGray = cvCreateImage(cvGetSize(prev), IPL_DEPTH_8U, 3);
+			cvCvtColor(flow,flowGray,CV_RGB2GRAY ); 
+
+			cvCalcOpticalFlowFarneback(im_gray_prev, im_gray_next, flow, 0.5, 1, 5, 5, 7, 1.5, 0);
+			// Buggy code ends here.. Too tired to look into it...
+			//cvCalcOpticalFlowFarneback(
+			//cvCalcOpticalFlowFarneback(previous, image, flow, 0.5, 1, 5, 5, 7, 1.5,  0);*/
+			//return 0;
 		}
-		printf("MORE Shit\n");
 	}
 	
 	// Clean up the webcam struct (stored in the heap)
 	cvReleaseCapture(&webcam);
 	
 	// Clean up the other structs
-	cvReleaseImage(&image);
+	cvReleaseImage(&next);
+	cvReleaseImage(&prev);
 	
 	return 0;
 }
